5113454e98fcd1ffb9889a63c3293346637e244c
ASaidOguz <ahmetsaid2136@gmail.com>
2024-12-15 01:17:00 +0300

    b-tree creation started.

---
+++ helper.go
@@ -0,0 +1,32 @@
+package main
+
+import "encoding/binary"
+
+// header
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node.data)
+}
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node.data[2:4])
+}
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node.data[0:2], btype)
+	binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
+}
+
+// pointers
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys())
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node.data[pos:])
+}
+func (node BNode) setPtr(idx uint16, val uint64) {
+	assert(idx < node.nkeys())
+	pos := HEADER + 8*idx
+	binary.LittleEndian.PutUint64(node.data[pos:], val)
+}
+func assert(condition bool) {
+	if !condition {
+		panic("assertion failed")
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+type BNode struct {
+	data []byte
+}
+
+type BTree struct {
+	root uint64 // pointer (a nonzero page number)
+
+	// callbacks for managing on-disk pages
+	get func(uint64) BNode // dereference a pointer
+	new func(BNode) uint64 // allocate a new page
+	del func(uint64)       // deallocate a page
+}
+
+const (
+	BNODE_NODE         = 1 // internal nodes without values
+	BNODE_LEAF         = 2 // leaf nodes with values
+	HEADER             = 4
+	BTREE_PAGE_SIZE    = 4096
+	BTREE_MAX_KEY_SIZE = 1000
+	BTREE_MAX_VAL_SIZE = 3000
+)
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	assert(node1max <= BTREE_PAGE_SIZE)
+}
+
 func main() {
 
 }
